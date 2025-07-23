4a6a8318aa6a27cbdaa65c7fe3fcab316adaa66a
livia.onofre <liviapso2012@gmail.com>
2025-07-23 19:05:24 -0300

    feat: adicionar componente Home com HTML, CSS e lógica de construção

---
+++ src/app/components/views/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { EmConstrucao } from "../../resources/em-construcao/em-construcao";
+
+@Component({
+  selector: 'app-home',
+  imports: [EmConstrucao],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home {
+
+}
