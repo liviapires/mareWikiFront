8e5a2b4b0c71a3aba3e933cc37de5aaf25bd9ba6
livia.onofre <liviapso2012@gmail.com>
2025-07-23 19:06:40 -0300

    feat: adicionar componente Sobre com HTML, CSS e lógica de construção

---
+++ src/app/components/views/outras-series/sobre/sobre.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { EmConstrucao } from "../../../resources/em-construcao/em-construcao";
+
+@Component({
+  selector: 'app-sobre',
+  imports: [EmConstrucao],
+  templateUrl: './sobre.html',
+  styleUrl: './sobre.css'
+})
+export class SobreOutrasSeries {
+
+}
