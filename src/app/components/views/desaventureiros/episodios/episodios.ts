aaa593a4400c53cbceb94fb2d7d20302f7888947
livia.onofre <liviapso2012@gmail.com>
2025-07-23 19:04:38 -0300

    feat: adicionar componente Episodios com HTML, CSS e lógica de construção

---
+++ src/app/components/views/desaventureiros/episodios/episodios.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { EmConstrucao } from "../../../resources/em-construcao/em-construcao";
+
+@Component({
+  selector: 'app-episodios',
+  imports: [EmConstrucao],
+  templateUrl: './episodios.html',
+  styleUrl: './episodios.css'
+})
+export class Episodios {
+
+}
