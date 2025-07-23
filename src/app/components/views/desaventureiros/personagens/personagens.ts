6ffa86675114631bc3395631bc152b29c5d57310
livia.onofre <liviapso2012@gmail.com>
2025-07-23 19:04:47 -0300

    feat: adicionar componente Personagens com HTML, CSS e lógica de construção

---
+++ src/app/components/views/desaventureiros/personagens/personagens.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { EmConstrucao } from "../../../resources/em-construcao/em-construcao";
+
+@Component({
+  selector: 'app-personagens',
+  imports: [EmConstrucao],
+  templateUrl: './personagens.html',
+  styleUrl: './personagens.css'
+})
+export class Personagens {
+
+}
