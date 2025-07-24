164ac36c8e435a4061818fa05a53994e9d4397c3
livia.onofre <liviapso2012@gmail.com>
2025-07-24 19:01:04 -0300

    feat: refatorar tabela de personagens para incluir filtro e ordenação

--- src/app/components/views/vinte-natural/tabela-de-personagens/tabela-de-personagens.ts
+++ src/app/components/views/vinte-natural/tabela-de-personagens/tabela-de-personagens.ts
@@ -1,29 +1,66 @@
-import { Component } from '@angular/core';
-import {MatTableModule} from '@angular/material/table';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSortModule, MatSort } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
+  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
+  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
+  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
+  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
+];
 @Component({
   selector: 'app-tabela-de-personagens',
   imports: [
-    MatTableModule
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSortModule,
+    MatPaginatorModule
   ],
   templateUrl: './tabela-de-personagens.html',
   styleUrl: './tabela-de-personagens.css'
 })
-export class TabelaDePersonagens {
-  personagens = [
-    { 
-      nome: 'Personagem 1', 
-      idade: 30, classe: 'Guerreiro' 
-    },
-    { 
-      nome: 'Personagem 2', 
-      idade: 25, classe: 'Mago' 
-    },
-    { 
-      nome: 'Personagem 3', 
-      idade: 28, classe: 'Arqueiro' 
-    }
-  ];
 
-  displayedColumns: string[] = ['nome', 'idade', 'classe'];
+export class TabelaDePersonagens implements AfterViewInit  {
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  @ViewChild(MatSort) sort!: MatSort;
+  
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }  
+
+  logData(row: PeriodicElement) {
+    console.log('Row data:', row);
+  }
+  
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+
 }
